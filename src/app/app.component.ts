9a695316d1aff8d10d34a2d7266498c7cb846108
dgschel <goesc_@hotmail.de>
2025-06-16 20:30:35 +0200

    remove angular directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
